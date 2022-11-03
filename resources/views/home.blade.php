7b7d134f768b0c675609e36ea8f4d830389b4f7d
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-03 20:29:55 +0100

    uso di bootstrap cdnjs e link css e js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Link Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Link JavaScript -->
+    <script type="text/javascript" src="{{ asset('js/app.js') }}" defer></script>
+
     <title>Trains</title>
 </head>
 
 <body>
 
-    {{-- Utilitzzo di data fittizia che combacia con i dati inseriti manualmente in phpMyAdmin --}}
     <?php
     $currentDate = date('Y-m-d');
     ?>
@@ -18,9 +31,7 @@
     <p>Data corrente: {{ $currentDate }} </p>
 
     @foreach ($trains as $train)
-        @if ($train['orario_partenza'] == $fakeCurrentDate)
-            <p>Azienda: {{ $train['azienda'] }} <span> | Orario partenza: {{ $train['orario_partenza'] }} </span> </p>
-        @endif
+        <p>Azienda: {{ $train['azienda'] }} <span> | Orario partenza: {{ $train['orario_partenza'] }} </span> </p>
     @endforeach
 
 </body>
