5ede54f9302618a59ae65f2c0f9fa7a73ac8bf7a
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-11-03 20:27:02 +0100

    uso di faker per creare 10 treni in db

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\models\Train;
+use Faker\Generator as Faker;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +12,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         //
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->name();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->isbn10();
+            $train->numero_carrozze = $faker->numberBetween(3, 12);
+            $train->puntuale = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
